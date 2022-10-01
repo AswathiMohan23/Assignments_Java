ec54df333f2e06fb2f4c8d9b04ba63ffcd4a5edf
AswathiMohan23 <achu999here@gmail.com>
2022-10-01 14:23:53 +0530

    [Aswathi] ADDED : a code to display a message

---
+++ DisplayAyMessage.java
@@ -0,0 +1,10 @@
+package com.java;
+
+import java.net.StandardSocketOptions;
+
+public class DisplayAyMessage {
+    public static void main(String[] args){
+        System.out.print("hello how are you");
+
+    }
+}
