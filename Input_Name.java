1e21debb69142a8e6f29d3a7234caff2221b96f8
AswathiMohan23 <achu999here@gmail.com>
2022-10-01 15:18:07 +0530

    [Aswathi] ADDED : a code to input a name

---
+++ Input_Name.java
@@ -0,0 +1,12 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Input_Name {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the name : ");
+        String name=sc.next();
+        System.out.println("the entered name is : "+name);
+    }
+}
