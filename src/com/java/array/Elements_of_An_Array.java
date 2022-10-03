3ee5057b072b6e1ac81194e61d3c043175096d67
AswathiMohan23 <achu999here@gmail.com>
2022-10-04 00:41:58 +0530

    [Aswathi] ADDED : a code to print elements in an array

---
+++ src/com/java/array/Elements_of_An_Array.java
@@ -0,0 +1,12 @@
+package com.java.array;
+
+public class Elements_of_An_Array {
+    public static void main(String args[]) {
+        //Syntax of arrays
+        int array[]={1,2,3,4};
+        int i;
+        for(i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
