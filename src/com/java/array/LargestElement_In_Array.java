6012c684538104e123bd2d4e416188fd7257c782
AswathiMohan23 <achu999here@gmail.com>
2022-10-04 00:50:25 +0530

    [Aswathi] ADDED : a code to 3) Java Program to print the largest element in an array

---
+++ src/com/java/array/LargestElement_In_Array.java
@@ -0,0 +1,24 @@
+package com.java.array;
+
+import java.util.Scanner;
+
+// 3) Java Program to print the largest element in an array
+//   =====================================================
+public class LargestElement_In_Array {
+    public static void main(String args[]) {
+        System.out.println("Enter the size of the array : ");
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int []inputArray=new int[size];    //Syntax of arrays
+        System.out.println("Enter the elements :");
+        for(int i=0; i<size; i++ )
+            inputArray[i] = sc.nextInt();
+        int largest = inputArray[0];
+        for (int i = 0; i < size; i++) {//inputArray) {
+            if (largest < inputArray[i])       //comparing the elements
+                largest = inputArray[i];
+        }
+        System.out.println("the largest array element is : "+largest);
+
+        }
+}
