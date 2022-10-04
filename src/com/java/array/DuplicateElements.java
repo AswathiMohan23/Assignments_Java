38769ea3081b9423a2f5348bf2c28bb15f6870d0
AswathiMohan23 <achu999here@gmail.com>
2022-10-04 10:19:44 +0530

    [Aswathi] ADDED : code to print duplicate elements of an array

--- src/com/java/array/DuplicateElements.java
+++ src/com/java/array/DuplicateElements.java
@@ -18,8 +18,7 @@ public class DuplicateElements {
         for(int i = 0; i < inputArray.length; i++) {
             for(int j = i + 1; j < inputArray.length; j++) {
                 if(inputArray[i] == inputArray[j])
-                    System.out.println(inputArray[j]);
-            }
+                    System.out.println(inputArray[j]);}
         }
     }
 }
