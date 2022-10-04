845ca81b7e6d147cb7231e678634d483cceca1b8
AswathiMohan23 <achu999here@gmail.com>
2022-10-04 09:47:25 +0530

    [Aswathi] ADDED : code to print the elements of an array present on the odd position

--- src/com/java/array/Elements_In_OddPosition.java
+++ src/com/java/array/Elements_In_OddPosition.java
@@ -3,22 +3,20 @@ package com.java.array;
 import java.util.Scanner;
 
 public class Elements_In_OddPosition {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("enter the size of an array : ");
-        Scanner sc= new Scanner(System.in);
-        int size=sc.nextInt();
-        int inputArray[]=new int[size];
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int inputArray[] = new int[size];
         System.out.println("enter the elements : ");
-        for(int i =0;i<size;i++){
-            inputArray[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            inputArray[i] = sc.nextInt();
         }
-        int odd[]=new int[size];
+        int odd[] = new int[size];
         System.out.println("the elements in the odd positions are : ");
-        for(int j=0;j<size;j++){
-            if(inputArray[j]%2!=0){
-                odd[j]=inputArray[j];
-                System.out.println("position "+j+" = "+odd[j]);
+        for (int j = 1; j < size; j = j+2) {
+            System.out.println("odd position " + j + " : element = " + inputArray[j]);
             }
         }
     }
-}
+
